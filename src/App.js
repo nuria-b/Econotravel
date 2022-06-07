f00093fabf5897111522dee90059d46d26e1a967
Jurki <natsuki.rin111@gmail.com>
2022-06-07 18:27:31 +0200

    #10 footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Fragment } from "react";
-import NavBar from "./components/NavBar.jsx";
-import GlobalStyle from "./globalStyles.jsx";
+import React, { Fragment } from 'react';
+import NavBar from './components/NavBar.jsx';
+import GlobalStyle from './globalStyles.jsx';
 import Search from './components/Search.jsx';
-import WhoWeAre from "./components/InfoFooter/WhoWeAre.jsx";
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <GlobalStyle/>
       <NavBar/>
       <Search/>
-      <WhoWeAre/>
+      <Footer/>
     </Fragment>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer(){
+    return(
+        <footer>
+            <a href='#'>¿Cómo funciona?</a>
+            <a href='/whoweare'>¿Quiénes somos?</a>
+            <a href='#'>Medios de pago</a>
+        </footer>
+    )
+}
