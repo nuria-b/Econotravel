5ba249137f230ba41aaab67e0cd34a196c1e2a9b
Jurki <natsuki.rin111@gmail.com>
2022-06-08 11:57:04 +0200

    #10 add routers

---
+++ src/components/InfoFooter/HowDoesItWork.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HowDoesItWork(){
+    return(
+        <>
+            <h1></h1>
+            <p>Página en construcción</p>
+        </>
+    )
+}
---
+++ src/components/InfoFooter/PaymentMethods.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function PaymentMethods(){
+    return(
+        <>
+            <h1>Medios de pago</h1>
+            <p>Página en construcción</p>
+        </>
+    )
+}
